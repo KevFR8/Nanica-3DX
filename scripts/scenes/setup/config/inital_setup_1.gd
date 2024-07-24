e5f1eab50f6bed369dcabd39d6d2f30ede1f9b02
KevFR <ikevfr@gmail.com>
2024-07-24 13:30:01 +0200

    new scenes

---
+++ scripts/scenes/setup/config/inital_setup_1.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_exit_button_pressed():
+	get_tree().change_scene_to_file("res://scene/setup/inital_setup_2.tscn")
+
+
+func _on_exit_button_text_pressed():
+	get_tree().change_scene_to_file("res://scene/setup/inital_setup_2.tscn")
