b3cd10b8d5ff6ba63cd7d9d7845d65d33faa7119
KevFR <ikevfr@gmail.com>
2024-11-14 21:42:35 +0100

    working a bit

--- scripts/scenes/main/ui/icons/icon_app_button.gd
+++ scripts/scenes/main/ui/icons/icon_app_button.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var anim = $"AnimationPlayer"
 
 func _ready():
-	anim.play("idle")
+	anim.play("inactive")
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("right"):
